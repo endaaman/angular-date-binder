28527885b9052a33bca7fb546c675989667ec34b
endaaman <buhibuhidog@gmail.com>
2014-12-24 14:44:18 +0900

    build

--- dist/angular-date-binder.js
+++ dist/angular-date-binder.js
@@ -4,47 +4,61 @@ angular.module('angularDateBinder', []).directive('bindDate', ["$parse", functio
     restrict: 'A',
     scope: true,
     link: function(scope, element, attrs) {
-      var applyDateObject, calcLastDate, current, equalDate, getter, refresh, setter, storeDateObject;
-      equalDate = function(d1, d2) {
-        return !!d1 && !!d2 && d1.getTime() === d2.getTime();
+      var applyDateObject, calcLastDate, current, equalDate, getter, refresh, setter, storeDateObject, validateDate, watchDate;
+      validateDate = function(date, abort) {
+        var retval;
+        retval = (date == null) || date.constructor === Date;
+        if (abort && !retval) {
+          throw new Error("Expected `" + attrs.bindDate + "` to be a date");
+        }
+        return retval;
       };
+      equalDate = function(d1, d2) {
+        return !!d1 && !!d2 && validateDate(d1) && validateDate(d2) && d1.getTime() === d2.getTime();
+      };
+      current = null;
       getter = $parse(attrs.bindDate);
       setter = $parse(attrs.bindDate).assign;
       storeDateObject = function() {
-        return getter(scope.$parent);
+        var date;
+        date = getter(scope.$parent);
+        validateDate(date, true);
+        return date;
       };
       applyDateObject = function(date) {
         setter(scope.$parent, date);
         return setter(scope, date);
       };
-      current = storeDateObject();
       calcLastDate = function(date) {
         return (new Date(date.getFullYear(), date.getMonth() + 1, 0)).getDate();
       };
-      refresh = function(updateDate) {
-        current = new Date(current.getTime());
-        scope.$year = current.getFullYear();
-        scope.$year = current.getFullYear();
-        scope.$month = current.getMonth() + 1;
-        scope.$date = current.getDate();
-        scope.$hours = current.getHours();
-        scope.$minutes = current.getMinutes();
-        scope.$seconds = current.getSeconds();
-        scope.$lastDate = calcLastDate(current);
-        return applyDateObject(current);
+      refresh = function(updates) {
+        if (current != null) {
+          if ((updates == null) && !updates) {
+            current = new Date(current.getTime());
+          }
+          scope.$year = current.getFullYear();
+          scope.$year = current.getFullYear();
+          scope.$month = current.getMonth() + 1;
+          scope.$date = current.getDate();
+          scope.$hours = current.getHours();
+          scope.$minutes = current.getMinutes();
+          scope.$seconds = current.getSeconds();
+          return scope.$lastDate = calcLastDate(current);
+        } else {
+          return scope.$year = scope.$year = scope.$month = scope.$date = scope.$hours = scope.$minutes = scope.$seconds = scope.$lastDate = null;
+        }
       };
-      scope.$parent.$watch(attrs.bindDate, function(newDateObject, oldDateObject) {
-        if (newDateObject && !equalDate(newDateObject, oldDateObject)) {
+      watchDate = function(newDateObject, oldDateObject) {
+        console.log('watch:');
+        console.log(newDateObject);
+        if ((newDateObject == null) || validateDate(newDateObject, true) && !equalDate(newDateObject, oldDateObject)) {
           current = newDateObject;
           return refresh();
         }
-      });
-      scope.$watch(attrs.bindDate, function(newDateObject, oldDateObject) {
-        if (newDateObject && !equalDate(newDateObject, oldDateObject)) {
-          current = newDateObject;
-          return refresh();
-        }
-      });
+      };
+      scope.$parent.$watch(attrs.bindDate, watchDate);
+      scope.$watch(attrs.bindDate, watchDate);
       scope.$watch('$year', function(newYear, oldYear) {
         if (newYear && newYear !== oldYear) {
           current.setFullYear(newYear);
@@ -81,6 +95,7 @@ angular.module('angularDateBinder', []).directive('bindDate', ["$parse", functio
           return refresh();
         }
       });
+      current = storeDateObject(false);
       return refresh();
     }
   };
